d6a8da030710aebdff0c253daafb17be9a28ba48
kowshi01 <kowshi0101@gmail.com>
2022-12-16 04:30:03 +0530

    5th commit

--- gitaddapp.js
+++ gitaddapp.js
@@ -1,12 +1,14 @@
-/*var a=document.getElementsByClassName("list-group-item");
-a[1].textContent='hello';
-a[2].style.backgroundColor = 'green';
-for(var i=0;i<a.length;i++){
-    a[i].style.fontWeight='bold';
-}*/
-var a=document.getElementsByTagName("li");
-a[1].textContent='hello';
-a[2].style.fontWeight='bold';
-for(var i=0;i<a.length;i++){
-    a[i].style.backgroundColor="pink";
+//Queryselector
+/*var secitem=document.querySelector('.list-group-item:nth-child(2)');
+secitem.style.backgroundColor='green';
+
+var thirditem=document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style="display:none";*/
+
+//QuerySelector all
+/*var secitem=document.querySelectorAll('li');
+secitem[1].style.backgroundColor='green';*/
+var items=document.querySelectorAll('li:nth-child(odd');
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor='green';
 }
