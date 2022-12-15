f3f32f3c6b62b9f1ba45b0a6896325bf1118034d
kowshi01 <kowshi0101@gmail.com>
2022-12-16 01:20:19 +0530

    4th commit

--- gitaddapp.js
+++ gitaddapp.js
@@ -1,6 +1,12 @@
-var a=document.getElementsByClassName("list-group-item");
+/*var a=document.getElementsByClassName("list-group-item");
 a[1].textContent='hello';
 a[2].style.backgroundColor = 'green';
 for(var i=0;i<a.length;i++){
     a[i].style.fontWeight='bold';
+}*/
+var a=document.getElementsByTagName("li");
+a[1].textContent='hello';
+a[2].style.fontWeight='bold';
+for(var i=0;i<a.length;i++){
+    a[i].style.backgroundColor="pink";
 }
